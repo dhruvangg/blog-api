28b6cd586e577dc8e466ea72a24ccbdc77e1a36d
Dhruvang Gajjar <dhruvangg@gmail.com>
2021-01-13 19:56:09 +0530

    API auth added

--- controller/user.js
+++ controller/user.js
@@ -1,11 +1,45 @@
-exports.createUser = async (req, res) => {
-    res.status(201).json({ "status": "ok", "message": "Create User" })
+const bcrypt = require("bcryptjs"),
+    jwt = require('jsonwebtoken'),
+    db = require("../models/index"),
+    JWT_SECRET = process.env.JWT_SECRET
+
+exports.signUp = async (req, res) => {
+    const { username, email, password: plainTextPassword } = req.body;
+    const password = await bcrypt.hash(plainTextPassword, 10)
+    try {
+        const user = await db.User.create({
+            username,
+            email,
+            password
+        })
+        delete user.password;
+        res.status(201).json({ "status": "ok", "message": user })
+    } catch (error) {
+        res.status(401).json({ "status": "error", "message": error.errors[0].message })
+    }
+}
+exports.signIn = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await db.User.findOne({ where: { email: email } })
+    if (!user) {
+        return res.status(401).json({ status: 'error', error: 'Invalid username/password' })
+    }
+    if (await bcrypt.compare(password, user.password)) {
+        const payload = { id: user.id, username: user.username };
+        const options = { expiresIn: '2d', issuer: 'http://localhost:8080' };
+        const secret = process.env.JWT_SECRET;
+        const token = jwt.sign(payload, secret, options)
+        return res.status(200).json({ status: 'ok', "message": "User signin successful", token })
+    }
+    return res.status(401).json({ "status": "error", "message": "Invalid Username/Password" })
 }
 exports.getUsers = async (req, res) => {
-    res.status(201).json({ "status": "ok", "message": "Get Users" })
-}
-exports.getUser = async (req, res) => {
-    res.status(201).json({ "status": "ok", "message": "Get User" })
+    try {
+        const users = await db.User.findAll()
+        res.status(201).json({ "status": "ok", "message": "Get User", users })
+    } catch (error) {
+        return res.status(401).json({ "status": "error", "message": error })
+    }
 }
 exports.removeUser = async (req, res) => {
     res.status(201).json({ "status": "ok", "message": "Remove User" })
--- router/user.js
+++ router/user.js
@@ -1,11 +1,14 @@
 const express = require('express'),
     router = express.Router(),
-    User = require('../controller/user')
+    User = require('../controller/user'),
+    checkAuth = require('../controller/utils').validateToken;
 
-router.get('/', User.getUsers);
-router.get('/:slug', User.getUser);
-router.post('/', User.createUser);
-router.delete('/:id', User.removeUser);
-router.put('/:id', User.updateUser);
+router.post('/register', User.signUp);
+router.post('/login', User.signIn);
+router.get('/users', checkAuth, User.getUsers);
+// router.get('/:slug', User.getUser);
+
+// router.delete('/:id', User.removeUser);
+// router.put('/:id', User.updateUser);
 
 module.exports = router
